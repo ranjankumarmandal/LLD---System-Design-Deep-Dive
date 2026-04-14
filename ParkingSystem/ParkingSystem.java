b3c87210e9ae5e6ea6ac26d0dc0af3642bbf3659
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-14 21:53:21 +0530

    LLD: Parking System: coding design: in-progress: created SpoVehicle

--- ParkingSystem/ParkingSystem.java
+++ ParkingSystem/ParkingSystem.java
@@ -4,3 +4,12 @@ enum SpotType {
     SMALL, MEDIUM, LARGE
 }
 
+abstract class Vehicle {
+    String licensePlate;
+    SpotType requiredSpotType;
+
+    public Vehicle(String licensePlate, SpotType requiredSpotType) {
+        this.licensePlate = licensePlate;
+        this.requiredSpotType = requiredSpotType;
+    }
+}
