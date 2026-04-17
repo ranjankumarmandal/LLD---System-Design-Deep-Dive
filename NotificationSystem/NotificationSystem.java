2ba6f0f6821d0b4f9e6f49937ff4580455ebfe7e
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-17 19:35:07 +0530

    LLD: NotificationSystem: in-progress: completed User

--- NotificationSystem/NotificationSystem.java
+++ NotificationSystem/NotificationSystem.java
@@ -32,4 +32,22 @@ class User {
     private String name;
     private String email;
     private String phone;
+
+    public User(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
