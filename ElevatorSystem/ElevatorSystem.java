eccbc71a7027156553be18cd4ee3284f3ca0bf24
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-19 22:20:49 +0530

    LLD: ElevatorSystem: in-progress: coding: created Request

--- ElevatorSystem/ElevatorSystem.java
+++ ElevatorSystem/ElevatorSystem.java
@@ -7,3 +7,13 @@ enum Direction {
 enum ElevatorState {
     MOVING, IDLE, STOPPED
 }
+
+class Request {
+    int floor;
+    Direction direction;
+
+    public Request(int floor, Direction direction) {
+        this.floor = floor;
+        this.direction = direction;
+    }
+}
