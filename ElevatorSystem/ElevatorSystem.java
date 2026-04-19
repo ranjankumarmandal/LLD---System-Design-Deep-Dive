4dc2968d32b3cbedf075bdee4e509b0a2bb5d620
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-19 22:21:25 +0530

    LLD: ElevatorSystem: in-progress: coding: created Elevator

--- ElevatorSystem/ElevatorSystem.java
+++ ElevatorSystem/ElevatorSystem.java
@@ -17,3 +17,53 @@ class Request {
         this.direction = direction;
     }
 }
+
+class Elevator {
+    int id;
+    int currentFloor;
+    TreeSet<Integer> targetFloors;
+    Direction direction;
+    ElevatorState state;
+
+    public Elevator(int id, int initialFloor) {
+        this.id = id;
+        this.currentFloor = initialFloor;
+        this.targetFloors = new TreeSet<>();
+        this.direction = Direction.IDLE;
+        this.state = ElevatorState.IDLE;
+    }
+
+    public void addTargetFloor(int floor) {
+        targetFloors.add(floor);
+        if (floor > currentFloor) direction = Direction.UP;
+        else if (floor < currentFloor) direction = Direction.DOWN;
+        else direction = Direction.IDLE;
+    }
+
+    public void step() {
+        if (state == ElevatorState.IDLE && !targetFloors.isEmpty()) {
+            state = ElevatorState.MOVING;
+        }
+        if (state == ElevatorState.MOVING) {
+            if (direction == Direction.UP) currentFloor++;
+            else if (direction == Direction.DOWN) currentFloor--;
+
+            if (targetFloors.contains(currentFloor)) {
+                targetFloors.remove(currentFloor);
+                System.out.println("Elevator " + id + " stopping at floor " + currentFloor);
+                state = ElevatorState.STOPPED;
+
+                if (targetFloors.isEmpty()) {
+                    direction = Direction.IDLE;
+                    state = ElevatorState.IDLE;
+                } else {
+                    int next = targetFloors.first();
+                    if (next > currentFloor) direction = Direction.UP;
+                    else if (next < currentFloor) direction = Direction.DOWN;
+                    else direction = Direction.IDLE;
+                    state = ElevatorState.MOVING;
+                }
+            }
+        }
+    }
+}
